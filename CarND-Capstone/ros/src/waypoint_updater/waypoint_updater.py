d9b9e8cb7a8fa13afd4f9c8c50dbd513bdb123d7
songshan0321 <songshan_you@hotmail.com>
2020-06-08 00:16:29 +0800

    Debug pose and waypoints check None

--- CarND-Capstone/ros/src/waypoint_updater/waypoint_updater.py
+++ CarND-Capstone/ros/src/waypoint_updater/waypoint_updater.py
@@ -49,7 +49,7 @@ class WaypointUpdater(object):
     def loop(self):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
-            if self.current_pose != None and self.base_waypoints != None:
+            if self.current_pose and self.base_waypoints and self.waypoints_tree:
                 # Get closest waypoint
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
                 self.publish_waypoints(closest_waypoint_idx)
